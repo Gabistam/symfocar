c97db3d8111b19a7f9227448f0ecd73726509c9e
Gabistam <48634603+Gabistam@users.noreply.github.com>
2023-07-22 00:24:48 +0200

    creation propriété stripeSessionId

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -45,6 +45,9 @@ class Order
     #[ORM\Column]
     private ?float $totalTTC = null;
 
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $stripeSessionId = null;
+
     public function __construct()
     {
         $this->orderDetails = new ArrayCollection();
@@ -187,6 +190,18 @@ class Order
     public function setReference(string $reference): self
     {
         $this->reference = $reference;
+
+        return $this;
+    }
+
+    public function getStripeSessionId(): ?string
+    {
+        return $this->stripeSessionId;
+    }
+
+    public function setStripeSessionId(?string $stripeSessionId): self
+    {
+        $this->stripeSessionId = $stripeSessionId;
 
         return $this;
     }
