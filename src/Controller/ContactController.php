5cfec3d866ea960538be8e1535d451327aa3a93a
Gabistam <48634603+Gabistam@users.noreply.github.com>
2023-08-01 09:28:01 +0200

    Ajout Page contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,17 +2,48 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Classe\Mail;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $content = "Bonjour,<br/><br/><br/>Vous avez reçu une nouvelle demande de contact depuis le site Symfocar.<br/><br/>";
+            $content .= "<br><hr><br><br><p style='text-align: center; font-weight: bold; text-decoration: underline;'>Voici les détails de la demande :</p><br/><br/><br/>";
+            $content .= "Nom : <b>".$form->get('firstname')->getData()."</b><br/><br/>";
+            $content .= "Prénom : <b>".$form->get('lastname')->getData()."</b><br/><br/>";
+            $content .= "Email : ".$form->get('email')->getData()."<br/><br/>";
+            $content .= "Téléphone : ".$form->get('phone')->getData()."<br/><br/>";
+            $content .= "<br/><b>Message :</b> <br/><br/><span style='font-style: italic; line-height: 1.5; font-weight: 300;'>".$form->get('message')->getData()."</span><br/><br/><br/><br/><br/>";
+            $content .= "<hr><br><br>A bientôt !";
+
+
+            $mail = new Mail();
+            $mail->send(
+                'gabistam@yahoo.fr',
+                'Symfocar',
+                'Vous avez reçu une nouvelle demande de contact',
+                $content
+            );
+
+            $this->addFlash('notice', 'Votre message a bien été envoyé !');
+
+            return $this->redirectToRoute('app_contact');
+            
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,15 +3,61 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre nom'
+                ],
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Entrez votre prénom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre email'
+                ]
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone',
+                'required' => false, // champ non obligatoire
+                'attr' => [
+                    'placeholder' => 'Entrez votre téléphone'
+                ]
+            ])
+            ->add('subject', TextType::class, [
+                'label' => 'Sujet',
+                'attr' => [
+                    'placeholder' => 'Entrez votre sujet'
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Message',
+                'attr' => [
+                    'placeholder' => 'Entrez votre message'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => [
+                    'class' => 'btn btn-primary current-btn col-12'
+                ]
+            ])
         ;
     }
 
